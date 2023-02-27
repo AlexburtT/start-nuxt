f3d55fcb8812eeb3274568ddf1f9c5edfdc14654
AlexburT <alexburt12634@gmail.com>
2023-02-27 23:14:43 +0900

    update nuxt.config

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <h1>Привет Мир!</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+	app: {
+		rootId: '__start',
+		rootTag: 'body',
+		head: {
+			htmlAttrs: {
+				lang: "ru"
+			},
+			title: "Start-Nuxt",
+			meta: [
+				{ charset: "utf-8" },
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+				{
+					name: "description",
+					content: "Start progect on the Nuxt 3",
+				},
+				{ name: "color-scheme", content: "light dark" },
+			],
+		}
+	},
+	css: ['~/assets/main.css']
 })
