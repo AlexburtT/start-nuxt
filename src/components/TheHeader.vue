3bc89e652785029390f7f3282f5193721e243bba
AlexburT <alexburt12634@gmail.com>
2023-03-01 00:11:17 +0900

    add useState

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,24 @@
 <template>
 	<header>
-		<ul>
+		<ul class="header-conteiner">
 			<li>
 				<NuxtLink to='/'>Main</NuxtLink>
 			</li>
 			<li>
 				<NuxtLink to='/about'>About</NuxtLink>
 			</li>
+			<li>
+				<NuxtLink to='/counter'>Counter</NuxtLink>
+			</li>
 		</ul>
 	</header>
 </template>
+
+<style>
+.header-conteiner {
+	display: flex;
+	gap: 2rem;
+	padding: 1rem;
+	background: var(--background);
+}
+</style>
--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -2,11 +2,13 @@
 	<div>
 		<h1>About pages</h1>
 		<img src="/tv.png" alt="tv" />
+		<p>Counter {{ counter }}</p>
 	</div>
 </template>
 
 <script setup>
 definePageMeta({
 	layout: 'custom',
-})
+});
+const counter = useCounter();
 </script>
